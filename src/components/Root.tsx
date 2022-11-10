6c56496a294ec8c203bcd9ee18a50489418e4c82
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-11-10 22:54:58 +0900

    header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,17 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import {FaAirbnb} from "react-icons/fa";
 
 export default function Root() {
   return <Box>
+    <HStack py={5} px={10} borderBottomWidth={1} justifyContent={"space-between"}>
+      <Box color={"red.500"}>
+    <FaAirbnb size={38} />
+    </Box> 
     <HStack>
-    <FaAirbnb />
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </HStack>
     </HStack>
     <Outlet />
   </Box>
